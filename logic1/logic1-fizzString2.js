af14948fc05fefe25884faf7097013a32811f8c2
JALoquacious <jalikoudis@protonmail.com>
2017-03-21 10:50:26 -0400

    Slight formatting mod to last upload

--- logic1/logic1-fizzString2.js
+++ logic1/logic1-fizzString2.js
@@ -13,7 +13,8 @@ function fizzString2(n) {
     return (n % 3 === 0 && n % 5 === 0)
         ? "FizzBuzz!" : (n % 3 === 0)
         ? "Fizz" : (n % 5 === 0)
-        ? "Buzz" : n + "!";
+        ? "Buzz" : n
+        + "!";
 }
 
 console.log(fizzString2(1)); // "1!"
