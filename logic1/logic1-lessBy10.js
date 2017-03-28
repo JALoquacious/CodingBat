6564f573d511064fdf66a5024dca04cedcaa2bd9
JALoquacious <jalikoudis@protonmail.com>
2017-03-28 09:40:28 -0400

    Added CodingBat LessBy10 exercise

---
+++ logic1/logic1-lessBy10.js
@@ -0,0 +1,18 @@
+/*
+Given three ints, a b c, return true if one of them is 10 or more less
+than one of the others.
+
+lessBy10(1, 7, 11) → true
+lessBy10(1, 7, 10) → false
+lessBy10(11, 1, 7) → true
+*/
+
+function lessBy10(a, b, c) {
+    return (Math.abs(a - b) >= 10) ||
+           (Math.abs(a - c) >= 10) ||
+           (Math.abs(b - c) >= 10);
+}
+
+console.log(lessBy10(1, 7, 11)); // true
+console.log(lessBy10(1, 7, 10)); // false
+console.log(lessBy10(11, 1, 7)); // true
