f0ef5c94e88135330af65d30d4f3b115d1acc760
JALoquacious <jalikoudis@protonmail.com>
2017-03-27 22:54:09 -0400

    Added CodingBat InOrder exercise

---
+++ logic1/logic1-inOrder.js
@@ -0,0 +1,19 @@
+/*
+Given three ints, a b c, return true if b is greater than a, and c is
+greater than b. However, with the exception that if "bool" is true, b
+does not need to be greater than a.
+
+inOrder(1, 2, 4, false) → true
+inOrder(1, 2, 1, false) → false
+inOrder(1, 1, 2, true) → true
+*/
+
+function inOrder(a, b, c, bool) {
+    return (bool)
+        ? (b < c)
+        : (a < b) && (b < c);
+}
+
+console.log(inOrder(1, 2, 4, false)); // true
+console.log(inOrder(1, 2, 1, false)); // false
+console.log(inOrder(1, 1, 2, true)); // true
