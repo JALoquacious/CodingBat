dc45118744c1f6de48ce868fe7bcda7509ef8732
JALoquacious <jalikoudis@protonmail.com>
2017-04-16 20:53:53 -0400

    Added CodingBat MakePi exercise

---
+++ array1/array1-makePi.js
@@ -0,0 +1,13 @@
+/*
+Return an int array length 3 containing the first 3 digits of pi,
+{3, 1, 4}.
+
+makePi() → [3, 1, 4]
+*/
+
+function makePi(array) {
+    const PI = [3, 1, 4];
+    return PI;
+}
+
+console.log(makePi()); // [3, 1, 4]
