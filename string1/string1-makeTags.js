6bfc9039bea9ba74be7088ecf1d4f04844c90404
JALoquacious <jalikoudis@protonmail.com>
2017-05-07 08:28:14 -0400

    Added CodingBat MakeTags exercise

---
+++ string1/string1-makeTags.js
@@ -0,0 +1,18 @@
+/*
+The web is built with HTML strings like "<i>Yay</i>" which draws Yay as
+italic text. In this example, the "i" tag makes <i> and </i> which
+surround the word "Yay". Given tag and word strings, create the HTML
+string with tags around the word, e.g. "<i>Yay</i>".
+
+makeTags("i", "Yay") → "<i>Yay</i>"
+makeTags("i", "Hello") → "<i>Hello</i>"
+makeTags("cite", "Yay") → "<cite>Yay</cite>"
+*/
+
+function makeTags(tag, txt) {
+    return `<${tag}>${txt}</${tag}>`;
+}
+
+console.log(makeTags("i", "Yay")); // "<i>Yay</i>"
+console.log(makeTags("i", "Hello")); // "<i>Hello</i>"
+console.log(makeTags("cite", "Yay")); // "<cite>Yay</cite>"
