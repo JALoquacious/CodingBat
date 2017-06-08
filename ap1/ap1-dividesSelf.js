29c02c75a9bfb18c7b915fb76d424e1b57c2eba4
JALoquacious <jalikoudis@protonmail.com>
2017-06-07 23:46:35 -0400

    Added CodingBat DividesSelf exercise

---
+++ ap1/ap1-dividesSelf.js
@@ -0,0 +1,27 @@
+/*
+We'll say that a positive int divides itself if every digit in the
+number divides into the number evenly. So for example 128 divides itself
+since 1, 2, and 8 all divide into 128 evenly. We'll say that 0 does not
+divide into anything evenly, so no number with a 0 digit divides itself.
+
+dividesSelf(128) → true
+dividesSelf(12) → true
+dividesSelf(120) → false
+*/
+
+function dividesSelf(n) {
+    let origN = n;
+    while (n > 0) {
+        let digit = n % 10;
+        if (origN % digit !== 0) {
+            return false;
+        }
+        n = parseInt(n / 10);
+    }
+    return true;
+}
+
+console.log(dividesSelf(128)); // true
+console.log(dividesSelf(12)); // true
+console.log(dividesSelf(120)); // false
+console.log(dividesSelf(124888)); // true
