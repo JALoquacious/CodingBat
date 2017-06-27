23a698541259c580434e3d7aa7f94a1c180e75a9
JALoquacious <jalikoudis@protonmail.com>
2017-06-27 04:57:04 -0400

    Added CodingBat PlusOut exercise

---
+++ string2/string2-plusOut.js
@@ -0,0 +1,17 @@
+/*
+Given a string and a non-empty word string, return a version of the
+original String where all chars have been replaced by pluses ("+"),
+except for appearances of the word string which are preserved unchanged.
+
+plusOut("12xy34", "xy") → "++xy++"
+plusOut("12xy34", "1") → "1+++++"
+plusOut("12xy34xyabcxy", "xy") → "++xy++xy+++xy"
+*/
+
+function plusOut(str, sub) {
+    return str.split(sub).map(el => el.replace(/./gi, '+')).join(sub);
+}
+
+console.log(plusOut("12xy34", "xy")); // "++xy++"
+console.log(plusOut("12xy34", "1")); // "1+++++"
+console.log(plusOut("12xy34xyabcxy", "xy")); // "++xy++xy+++xy"
