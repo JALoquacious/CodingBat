c76a1ff10329de8855d876e023a9f336ab6b1fa4
JALoquacious <jalikoudis@protonmail.com>
2017-07-15 11:36:41 -0400

    Added CodingBat BunnyEars exercise

---
+++ recursion1/recursion1-bunnyEars.js
@@ -0,0 +1,18 @@
+/*
+We have a number of bunnies and each bunny has two big floppy ears. We
+want to compute the total number of ears across all the bunnies
+recursively (without loops or multiplication).
+
+bunnyEars(0) → 0
+bunnyEars(1) → 2
+bunnyEars(2) → 4
+*/
+
+function bunnyEars(n) {
+	return (n === 0) ? 0 : 2 + bunnyEars(n - 1);
+}
+
+console.log(bunnyEars(0)); // 0
+console.log(bunnyEars(1)); // 2
+console.log(bunnyEars(2)); // 4
+console.log(bunnyEars(15)); // 30
