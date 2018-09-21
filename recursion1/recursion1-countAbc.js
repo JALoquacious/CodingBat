a021bae061a0869e3ffc2dfe4b829059fa8594d5
JALoquacious <jalikoudis@protonmail.com>
2018-09-21 14:38:02 -0400

    Added CodingBat CountABC exercise

---
+++ recursion1/recursion1-countAbc.js
@@ -0,0 +1,25 @@
+/*
+Count recursively the total number of "abc" and "aba" substrings that
+appear in the given string.
+
+countAbc("abc") → 1
+countAbc("abcxxabc") → 2
+countAbc("abaxxaba") → 2
+*/
+
+function countAbc(str) {
+    if (str.length === 0) {
+		return 0;
+    } else {
+    	let sub = str.substr(0, 3);
+		return (sub === "abc" || sub === "aba")
+			? 1 + countAbc(str.substr(sub.length))
+			: countAbc(str.substr(1));
+    }
+}
+
+console.log(countAbc("abc")); // 1
+console.log(countAbc("abcxxabc")); // 2
+console.log(countAbc("abaxxaba")); // 2
+console.log(countAbc("abcabacbacbacbabacabaccab")); // 4
+console.log(countAbc("1abc2aba3abc4aba5ccc6bab7cac8bcb9abc")); // 5
